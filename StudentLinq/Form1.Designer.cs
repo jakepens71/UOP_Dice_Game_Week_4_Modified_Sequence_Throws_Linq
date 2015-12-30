d065c5543d13fc937c0e2205e3230cbc151bc539
Jacob Shay <jake_ps@comcast.net>
2015-12-30 08:51:12 -0500

    Added firstName, LastName, grades to student class. Added get/set methods for first and last name.

--- StudentLinq/Form1.Designer.cs
+++ StudentLinq/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 443);
+            this.ClientSize = new System.Drawing.Size(572, 335);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
---
+++ StudentLinq/Students.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLinq
+{
+    class Students
+    {
+        string firstName = "";
+        string lastName = "";
+        List<int> grades = new List<int>();
+
+        /*
+        *
+        *Create generic constructor to intailize students
+        *
+        */
+        public Students()
+        {
+
+        }
+
+        /*
+        *
+        *Create consturctor to initlize students with a first name, last name
+        *@param firstName-The first name of the student. String type.
+        *@param lastName-The last name of the student. String type.
+        */
+        public Students(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        /*
+        *
+        *Creates constructor to initalize student with a first name, last name, and a grade
+        *@param firstName-The first name of the student. String type.
+        *@param lastName-The last name of the student. String type.
+        *@param grade-A grade for the student. Gets added to the list of grades. Integer type
+        *
+        */
+        public Students(string firstName, string lastName, int grade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            grades.Add(grade);
+        }
+
+        /*
+        *Sets the firstName of the student
+        *@param firstName-The first name of the student. String type
+        */
+        public void setFirstName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        /*
+        *Gets the students firstName
+        *
+        */
+        public string getFirstName()
+        {
+            return firstName;
+        }
+
+        /*
+        *Sets the lastName of the student
+        *@param lastName-The last name of the student. String type
+        */
+        public void setLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        /*
+        *Gets the students lastName
+        *
+        */
+        public string getLastName()
+        {
+            return lastName;
+        }
+
+    }
+}
